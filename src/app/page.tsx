8a3a836f943f64ce72624b8a0116c12c147870f5
MuggsyCodes <avilac@stanfordalumni.org>
2025-05-07 19:31:32 -0500

    page.tsx update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Explanatory from "@/components/sections/Explanatory";
 import GuidedOptimization from "@/components/sections/GuidedOptimization";
 //import FeaturedOn from "@/components/sections/FeaturedOn";
 import { ScrollButtons } from "@/components/sections/ButtonCarousel";
-import Call2Action from "@/components/sections/Call2Action";
+// import Call2Action from "@/components/sections/Call2Action";
 import { CarouselSpacing } from "@/components/sections/OptimizeCard";
 import EducationSection from "@/components/sections/EducationSection";
 //import Process from "@/components/Process";
